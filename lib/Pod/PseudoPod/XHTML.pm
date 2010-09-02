b34ce732bb47c59e070355e6a11d4ca647892665
Harley Pig <harleypig@gmail.com>
2010-09-01 20:09:04 -0600

    started work on actual code, fixed documentation

--- lib/Pod/PseudoPod/XHTML.pm
+++ lib/Pod/PseudoPod/XHTML.pm
@@ -3,9 +3,11 @@ package Pod::PseudoPod::XHTML;
 use warnings;
 use strict;
 
+use base qw( Pod::PseudoPod::HTML );
+
 =head1 NAME
 
-Pod::PseudoPod::XHTML - The great new Pod::PseudoPod::XHTML!
+Pod::PseudoPod::XHTML -- format PseudoPod as valid XHTML
 
 =head1 VERSION
 
@@ -18,34 +20,42 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  use Pod::PseudoPod::XHTML;
 
-Perhaps a little code snippet.
+  my $parser = Pod::PseudoPod::XHTML->new();
 
-    use Pod::PseudoPod::XHTML;
+  ...
 
-    my $foo = Pod::PseudoPod::XHTML->new();
-    ...
+  $parser->parse_file('path/to/file.pod');
+
+=head1 DESCRIPTION
+
+This class is a formatter that takes PseudoPod and renders it as
+valid XHTML.
+
+This is a subclass of L<Pod::PseudoPod::HTML>, and from there
+L<Pod::PseudoPod>, and inherits all their methods.
+
+This code has been shamelessly ripped off from L<Pod::PseudoPod::HTML> and
+jmcnamara's work on the Modern Perl epub book generator and massaged to work.
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Nothing is exported.
 
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
+=head1 METHODS
 
 =cut
 
-sub function1 {
-}
+sub new {
 
-=head2 function2
+  my $self = shift;
+  my $new  = $self->SUPER::new( @_ );
 
-=cut
+  $new->accept_targets( 'xhtml', 'XHTML' );
 
-sub function2 {
+  return $new;
+
 }
 
 =head1 AUTHOR
@@ -54,12 +64,15 @@ Alan Young, C<< <alansyoungiii at gmail.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-pod-pseudopod-xhtml at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Pod-PseudoPod-XHTML>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+This project is hosted on github
+(L<http://github.com/harleypig/Pod-PseudoPod-XHTML>).  I'll see any issues
+submitted there much faster than anywhere else.
 
-
-
+You may also report any bugs or feature requests to C<bug-pod-pseudopod-xhtml at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Pod-PseudoPod-XHTML>.  I will
+be notified, and then you'll automatically be notified of progress on your bug
+as I make changes.
 
 =head1 SUPPORT
 
@@ -67,10 +80,13 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Pod::PseudoPod::XHTML
 
-
 You can also look for information at:
 
 =over 4
+
+=item * github
+
+L<http://github.com/harleypig/Pod-PseudoPod-XHTML>
 
 =item * RT: CPAN's request tracker
 
@@ -90,9 +106,9 @@ L<http://search.cpan.org/dist/Pod-PseudoPod-XHTML/>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
 
+jmcnamara, Allison Randall, Larry Wall, the whole perl community
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -103,7 +119,6 @@ under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
-
 
 =cut
 
